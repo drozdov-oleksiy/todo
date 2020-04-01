1decf4c7114cf8f1e236b929a1c8de56d9a8519d
Oleksiy Drozdov <oleksiydrozdov@gamil.com>
2020-04-01 20:47:12 +0200

    add varible input

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const form = document.querySelector("form.addElement");
+const input = document.querySelector(".addElement input");
 
 const addTask = e => {
   e.preventDefault();
-  console.log("hello");
+  console.log(input.value);
 };
 
 form.addEventListener("submit", addTask);
