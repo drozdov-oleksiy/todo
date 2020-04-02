02ae6d04199a59a735f54fec5243d2ac9db0996f
Oleksiy Drozdov <oleksiydrozdov@gamil.com>
2020-04-02 15:07:33 +0200

    add function update

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const form = document.querySelector("form.addElement");
 const input = document.querySelector(".addElement input");
+const ul = document.querySelector("ul.container");
+const tasks = [];
+
+const update = () => {
+  tasks.forEach((task, i) => {
+    task.dataset.key = i;
+    ul.appendChild(task);
+  });
+  input.value = "";
+};
 
 const addTask = e => {
   e.preventDefault();
-  console.log(input.value);
+  if (input.value === "") return;
+  const li = document.createElement("li");
+  li.innerHTML = input.value + " <button>usun</button>";
+  tasks.push(li);
+  update();
 };
 
 form.addEventListener("submit", addTask);
